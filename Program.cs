cd0f8fb748571651dcdda148f46e692dab2c085c
microchelick43018 <dyatlovkonstan@gmail.com>
2021-02-24 00:59:34 +0300

    123321

--- Program.cs
+++ Program.cs
@@ -4,28 +4,6 @@ namespace PlayersDataBase
 {
     class Program
     {
-        static int ReadInt()
-        {
-            bool isCorrected = int.TryParse(Console.ReadLine(), out int choice);
-            while (isCorrected == false)
-            {
-                Console.WriteLine("Неверный ввод. Повторите попытку: ");
-                isCorrected = int.TryParse(Console.ReadLine(), out choice);
-            }
-            return choice;
-        }
-
-        static int MakeChoice(int maxNumber = 5)
-        {
-            int choice = ReadInt();
-            while (choice > maxNumber || choice < 1)
-            {
-                Console.Write("Неверный ввод. Повторите попытку: ");
-                choice = ReadInt();
-            }
-            return choice;
-        }
-
         static void ShowMenu()
         {
             Console.WriteLine("Выберите одну из функций:");
@@ -39,12 +17,13 @@ namespace PlayersDataBase
         static void Main(string[] args)
         {
             DataEditor dataEditor = new DataEditor();
+            InputChecker inputChecker = new InputChecker();
             int choice;
             bool exit = false;
             while (exit == false)
             {
                 ShowMenu();
-                choice = MakeChoice();
+                choice = inputChecker.MakeChoice();
                 switch (choice)
                 {
                     case 1:
@@ -70,127 +49,158 @@ namespace PlayersDataBase
                 Console.Clear();
             }
         }
+    }
 
-        class DataEditor
+    class InputChecker
+    {
+        public int ReadInt()
         {
-            private List<Player> _players;
-
-            public DataEditor()
+            bool isCorrected = int.TryParse(Console.ReadLine(), out int choice);
+            while (isCorrected == false)
             {
-                _players = new List<Player>();
+                Console.WriteLine("Неверный ввод. Повторите попытку: ");
+                isCorrected = int.TryParse(Console.ReadLine(), out choice);
             }
+            return choice;
+        }
 
-            public void ShowPlayers()
+        public int MakeChoice(int maxNumber = 5)
+        {
+            int choice = ReadInt();
+            while (choice > maxNumber || choice < 1)
             {
-                for (int i = 0; i < _players.Count; i++)
-                {
-                    _players[i].ShowInfo(i + 1);
-                }
+                Console.Write("Неверный ввод. Повторите попытку: ");
+                choice = ReadInt();
             }
+            return choice;
+        }
+    }
 
-            public void AddPlayer()
-            {
-                string nickName;
-                int level;
-                bool isBanned;
-                Console.WriteLine("Введите Никнейм игрока: ");
-                nickName = Console.ReadLine();
-                Console.WriteLine("Введите уровень игрока: ");
-                level = ReadInt();
-                Console.WriteLine("Забанен ли игрок? 1 - да, 2 - нет.");
-                int choice = ReadInt();
-                while (choice != 1 && choice != 2)
-                {
-                    Console.WriteLine("Неверный ввод. Повторите попытку: ");
-                    choice = ReadInt();
-                }
-                isBanned = choice == 1;
-                Player player = new Player(nickName, level, isBanned);
-                _players.Add(player);
-                Console.WriteLine("Игрок добавлен!");                
-            }
+    class DataEditor
+    {
+        private List<Player> _players;
+        private static InputChecker _inputChecker;
 
-            public void BlockPlayer()
-            {
-                if (CountChecker() == true)
-                {
-                    ShowPlayers();
-                    Console.Write("Введите номер игрока, которого хотите забанить: ");
-                    int choice = MakeChoice(_players.Count);
-                    _players[choice - 1].Block();
-                }
-            }
+        public DataEditor()
+        {
+            _players = new List<Player>();
+        }
 
-            public void UnblockPlayer()
-            {
-                if (CountChecker() == true)
-                {
-                    ShowPlayers();
-                    Console.Write("Введите номер игрока, которого хотите разбанить: ");
-                    int choice = MakeChoice(_players.Count);
-                    _players[choice - 1].UnBlock();
-                }
-            }
+        static DataEditor()
+        {
+            _inputChecker = new InputChecker();
+        }
 
-            public void DeletePlayer()
+        public void ShowPlayers()
+        {
+            for (int i = 0; i < _players.Count; i++)
             {
-                if (CountChecker() == true)
-                {
-                    ShowPlayers();
-                    Console.Write("Введите номер игрока, которого хотите удалить: ");
-                    int choice = MakeChoice(_players.Count);
-                    _players.RemoveAt(choice - 1);
-                }
-            }
-
-            public bool CountChecker()
-            {
-                if (_players.Count == 0)
-                {
-                    Console.WriteLine("Листа не существует! Добавьте хотя бы одного игрока.");
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
+                _players[i].ShowInfo(i + 1);
             }
         }
 
-        class Player
+        public void AddPlayer()
         {
-            public string NickName { get; private set; }
-            public int Level { get; private set; }
-            public bool IsBanned { get; private set; }
-
-            public Player(string nickName, int level, bool isBanned)
+            string nickName;
+            int level;
+            bool isBanned;
+            Console.WriteLine("Введите Никнейм игрока: ");
+            nickName = Console.ReadLine();
+            Console.WriteLine("Введите уровень игрока: ");
+            level = _inputChecker.ReadInt();
+            Console.WriteLine("Забанен ли игрок? 1 - да, 2 - нет.");
+            int choice = _inputChecker.ReadInt();
+            while (choice != 1 && choice != 2)
             {
-                NickName = nickName;
-                Level = level;
-                IsBanned = isBanned;
-            }       
-
-            public void Block()
-            {
-                IsBanned = true;
+                Console.WriteLine("Неверный ввод. Повторите попытку: ");
+                choice = _inputChecker.ReadInt();
             }
+            isBanned = choice == 1;
+            Player player = new Player(nickName, level, isBanned);
+            _players.Add(player);
+            Console.WriteLine("Игрок добавлен!");
+        }
 
-            public void UnBlock()
+        public void BlockPlayer()
+        {
+            if (CountChecker() == true)
             {
-                IsBanned = false;
+                ShowPlayers();
+                Console.Write("Введите номер игрока, которого хотите забанить: ");
+                int choice = _inputChecker.MakeChoice(_players.Count);
+                _players[choice - 1].Block();
             }
+        }
 
-            public void ShowInfo(int orderNumber)
+        public void UnblockPlayer()
+        {
+            if (CountChecker() == true)
             {
-                Console.Write($"Номер: {orderNumber}, никнейм: {NickName}, уровень: {Level}, cтатус - ");
-                if (IsBanned == true)
-                {
-                    Console.WriteLine("забанен");
-                }
-                else
-                { 
-                    Console.WriteLine("не забанен"); 
-                }
+                ShowPlayers();
+                Console.Write("Введите номер игрока, которого хотите разбанить: ");
+                int choice = _inputChecker.MakeChoice(_players.Count);
+                _players[choice - 1].UnBlock();
+            }
+        }
+
+        public void DeletePlayer()
+        {
+            if (CountChecker() == true)
+            {
+                ShowPlayers();
+                Console.Write("Введите номер игрока, которого хотите удалить: ");
+                int choice = _inputChecker.MakeChoice(_players.Count);
+                _players.RemoveAt(choice - 1);
+            }
+        }
+
+        public bool CountChecker()
+        {
+            if (_players.Count == 0)
+            {
+                Console.WriteLine("Листа не существует! Добавьте хотя бы одного игрока.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+    class Player
+    {
+        public string NickName { get; private set; }
+        public int Level { get; private set; }
+        public bool IsBanned { get; private set; }
+
+        public Player(string nickName, int level, bool isBanned)
+        {
+            NickName = nickName;
+            Level = level;
+            IsBanned = isBanned;
+        }
+
+        public void Block()
+        {
+            IsBanned = true;
+        }
+
+        public void UnBlock()
+        {
+            IsBanned = false;
+        }
+
+        public void ShowInfo(int orderNumber)
+        {
+            Console.Write($"Номер: {orderNumber}, никнейм: {NickName}, уровень: {Level}, cтатус - ");
+            if (IsBanned == true)
+            {
+                Console.WriteLine("забанен");
+            }
+            else
+            {
+                Console.WriteLine("не забанен");
             }
         }
     }
